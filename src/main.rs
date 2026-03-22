69c7e2edf2dcb45db84f5b455f4e775d181ef032
Tawxyn <oleshchukmike@gmail.com>
2026-03-21 20:44:20 -0700

    Get input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    loop {
+        print!("db> ");
+        io::stdout().flush().unwrap();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let command = input.trim();
+                println!("{}", command);
+            }
+            Err(error) => println!("{}", error),
+        }
+    }
 }
